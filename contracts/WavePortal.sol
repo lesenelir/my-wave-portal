2a177b4cf7f548013b5d3a7ed0f6a20585961cc0
lesenelir <miaomiaobabyzy@gmail.com>
2022-12-06 20:04:56 +0800

    feat: add 20 address to wave us

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor(){
         console.log("Yo yo, i am a smart contract, what about you ?");
     }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves.", totalWaves);
+        return totalWaves;
+    }
+
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,42 @@
 const hre = require('hardhat')
 
 const main = async  () => {
+  // const [owner, randomPerson, randomPerson2] = await hre.ethers.getSigners()
+  // 对象数组，每一个对象有address属性
+  const signers = await hre.ethers.getSigners()
+  const waveAddressArray = []
+
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
   const waveContract = await waveContractFactory.deploy()
   await waveContract.deployed()
 
-  console.log('Contract deployed to: ', waveContract.address)
+  console.log('Contract deployed to: ', waveContract.address) // 合约地址
+  console.log('Contract deployed by: ', signers[0].address) // 创建合约的地址就是signer的第一个地址
+
+  await waveContract.getTotalWaves()
+
+  const firstWaveTxn = await waveContract.wave()
+  await firstWaveTxn.wait()
+  await waveContract.getTotalWaves()
+  waveAddressArray.push(firstWaveTxn.from)
+
+  for (let i = 1; i < signers.length; i++) {
+    const tempWaveTxn = await waveContract.connect(signers[i]).wave()
+    await tempWaveTxn.wait()
+    await waveContract.getTotalWaves()
+    waveAddressArray.push(tempWaveTxn.from)
+  }
+
+  console.log('All wave addresses: ', waveAddressArray)
+
+  // const secondWaveTxn = await waveContract.connect(signers[1]).wave()
+  // await secondWaveTxn.wait()
+  // await waveContract.getTotalWaves()
+  //
+  // const thirdWaveTxn = await waveContract.connect(randomPerson2).wave()
+  // await thirdWaveTxn.wait()
+  // await waveContract.getTotalWaves()
+
 }
 
 const runMain = async () => {
