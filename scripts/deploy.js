2b32029e5a92286c6061cae53ba216ee7aacf605
lesenelir <miaomiaobabyzy@gmail.com>
2022-12-06 20:38:00 +0800

    feat: deploy contract to localhost chain

---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+const hre = require('hardhat')
+
+const main = async () => {
+  const [deployer] = await hre.ethers.getSigners()
+  const accountBalance = await deployer.getBalance()
+
+  console.log('Deploying contracts with account: ', deployer.address)
+  console.log('Account balance: ', accountBalance.toString())
+
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
+  const waveContract = await waveContractFactory.deploy()
+  await waveContract.deployed()
+
+  console.log('WavePortal address: ', waveContract.address)
+}
+
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (e) {
+    console.log(e)
+    process.exit(1)
+  }
+}
+
+runMain().then(r => console.log(r))
