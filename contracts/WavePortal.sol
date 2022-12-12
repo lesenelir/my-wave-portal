f61d7d12a372332d1365388c0085ca95fdd5e6d9
lesenelir <miaomiaobabyzy@gmail.com>
2022-12-12 17:06:04 +0800

    feat: add seed win to get prizeAmount

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -7,6 +7,9 @@ contract WavePortal {
     // test3
     uint256 totalWaves;
 
+    // generate a random number
+    uint256 private seed;
+
     event NewWave(address indexed from, uint256 timestamp, string message);
 
     struct Wave {
@@ -20,6 +23,8 @@ contract WavePortal {
 
     constructor() payable {
         console.log("Yo yo, i am a smart contract, what about you ?");
+        // set initial seed
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
@@ -28,12 +33,27 @@ contract WavePortal {
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
+        // generate a new seed for the next user that seed a wave
+        seed = (block.timestamp + block.difficulty + seed) % 100;
+        console.log("Random # generated: %d", seed);
+
+        // give 50% chance that the user wins the price
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            // the same code we had before to send the prize
+            uint256 prizeAmount = 0.0001 ether;
+            require(prizeAmount <= address(this).balance, "Trying to withdraw more money than the contract has.");
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
         emit NewWave(msg.sender, block.timestamp, _message);
 
-        uint256 prizeAmount = 0.0001 ether;
-        require(prizeAmount <= address(this).balance, "Trying to withdraw more money than the contract has.");
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdraw money from contract.");
+//        uint256 prizeAmount = 0.0001 ether;
+//        require(prizeAmount <= address(this).balance, "Trying to withdraw more money than the contract has.");
+//        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+//        require(success, "Failed to withdraw money from contract.");
     }
 
 
